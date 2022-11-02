51d87e2ed5b841567b25f9fa72ab315c0f707bca
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-11-02 08:47:42 -0500

    setting up enemy characters

--- js/index.js
+++ js/index.js
@@ -10,5 +10,7 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image();
 image.onload = () => {
   c.drawImage(image, 0, 0);
+  c.fillStyle = "red";
+  c.fillRect(100, 100, 100, 100);
 };
 image.src = "img/gameMap.png";
