0e273dab1e51468e2cba5cf093f3ad0de599fc4f
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-11-30 04:47:20 -0600

    working on building sprites

--- js/index.js
+++ js/index.js
@@ -56,6 +56,7 @@ let activeTile = undefined;
 let enemyCount = 3;
 let hearts = 10;
 let coins = 100;
+const explosions = [];
 spawnEnemies(enemyCount);
 
 function animate() {
@@ -77,6 +78,16 @@ function animate() {
         cancelAnimationFrame(animationId);
         document.querySelector("#gameOver").style.display = "flex";
       }
+    }
+  }
+
+  for (let i = explosions.length - 1; i >= 0; i--) {
+    const explosion = explosions[i];
+    explosion.draw();
+    explosion.update();
+
+    if (explosion.frames.current >= explosion.frames.max - 1) {
+      explosions.splice(i, 1);
     }
   }
 
@@ -126,7 +137,14 @@ function animate() {
           }
         }
 
-        // console.log(projectile.enemy.health);
+        explosions.push(
+          new Sprite({
+            position: { x: projectile.position.x, y: projectile.position.y },
+            imageSrc: "./img/explosion.png",
+            frames: { max: 4 },
+            offset: { x: 0, y: 0 },
+          })
+        );
         building.projectiles.splice(i, 1);
       }
     }
